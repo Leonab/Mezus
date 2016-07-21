31b2f184ee8197422ed9c5a1be2196b708ef79b3
Nabeel Khawar <Nabeel Khawar>
2016-07-21 21:08:47 +0530

    build for the future

---
+++ app/routes.js
@@ -0,0 +1,68 @@
+var Todo = require('./models/todo');
+var multer  = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+
+
+
+/*function getTodos(res) {
+    Todo.find(function (err, todos) {
+
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(todos); // return all todos in JSON format
+    });
+}
+;
+*/
+module.exports = function (app) {
+
+    // api ---------------------------------------------------------------------
+    // get all todos
+    app.get('/api/todos', function (req, res) {
+        // use mongoose to get all todos in the database
+        getTodos(res);
+    });
+
+    // create todo and send back all todos after creation
+    app.post('/api/add', function(req, res){
+    var upload = multer({ storage: storage }).single('audio');
+    console.log("yolo");
+    upload(req, res, function (err) {
+    if (err) {
+      // An error occurred when uploading
+      console.log(err);
+      return
+    }
+  console.log(req.body);
+  console.log(req.file);
+    })
+  });
+
+
+    // delete a todo
+    app.delete('/api/todos/:todo_id', function (req, res) {
+        Todo.remove({
+            _id: req.params.todo_id
+        }, function (err, todo) {
+            if (err)
+                res.send(err);
+
+            getTodos(res);
+        });
+    });
+
+    // application -------------------------------------------------------------
+    app.get('*', function (req, res) {
+        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+};
---
+++ public/js/services/todos.js
@@ -0,0 +1,17 @@
+angular.module('todoService', [])
+
+	// super simple service
+	// each function returns a promise object
+	.factory('Song', ['$http',function($http) {
+		return {
+			get : function() {
+				return $http.get('/api/todos');
+			},
+			add : function() {
+				return $http.post('/api/add');
+			},
+			delete : function(id) {
+				return $http.delete('/api/todos/' + id);
+			}
+		}
+	}]);
